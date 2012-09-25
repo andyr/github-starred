0fcd22cab963097fda7f0fc9cb608be004706067
Andy Ramakrishna <andyr.dev@gmail.com>
2012-09-25 03:34:30 -0700

    Adding package structure plus some statics

---
+++ src/github_api.py
@@ -0,0 +1,27 @@
+
+from datetime import datetime
+from github import Github, GithubException
+import json
+import logging
+
+
+
+logger = logging.getLogger(__name__)
+
+# convention for importing from __init__.py
+#oauth_token = 'asdf'
+
+# establish client connection, fetch content
+# memoize response (across requests on a running server)
+user = 'andyr'
+starred = Github().get_user(user).get_starred()
+
+
+# format response
+[{'name': repo.name,
+  'description':repo.description,
+  'url':repo.git_url
+} for repo in starred]
+
+
+# render content
---
+++ static/readme.md
@@ -0,0 +1,7 @@
+static files
+=============
+
+kizzy - cross browser localStorage interface
+backbone - mvc design pattern for client
+underscore - utility functions (refer to these from google code *or* use mod_cache or some such static server)
+
